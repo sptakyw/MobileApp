7aad50a30f6a81c4b53f7e4668f5863496f2eecb
sptakyw <6206021620141@fitm.kmutnb.ac.th>
2022-09-03 23:16:56 +0700

    SAVE

---
+++ assignment8_ufo/app/src/main/java/com/example/assignment8_ufo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.assignment8_ufo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GraphicsView(this));
+    }
+}
